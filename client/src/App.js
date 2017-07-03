f27efb389cc4530359c3dbcabe9ddaab6e7a321a
Amy Dredge <amyadredge@gmail.com>
2017-07-03 10:31:10 -0600

    Make simple changes to main view

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    lists: [{name: "List 1", id: 1}, {name: "List 2", id: 2}]
+  };
+
   render() {
+
+    const listNameItems = this.state.lists.map(list => (
+      <li>{list.name}</li>
+    ));
+
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Your lists</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+      <ul>
+        {listNameItems}
+      </ul>
       </div>
     );
   }
